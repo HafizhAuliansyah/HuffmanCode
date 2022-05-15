9e1ae690d1acabdd58e379ff62aeb03649563938
salmanalfarisi29 <salmannalfarisii994@gmail.com>
2022-05-15 16:44:09 +0700

    menambahkan modul help

---
+++ openHelp.cpp
@@ -0,0 +1,17 @@
+void openHelp(){
+	FILE *data;
+	char panduan[600];
+	
+	data = fopen("help.txt", "r");
+	
+	system("cls");
+	while(!feof(data)){
+		fgets(panduan, sizeof(panduan), data);
+		printf("%s", panduan);
+	}
+	
+	printf("\n\t");
+	fclose(data);
+	system("pause");
+	main();
+}
